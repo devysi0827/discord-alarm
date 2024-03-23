d82c911a795e5cb117d328f01003f9a6e39218de
devysi0827 <devysi0827@gmail.com>
2024-03-23 23:55:49 +0900

    [feat] 함수형 전환

--- alarm.js
+++ alarm.js
@@ -1,26 +1,35 @@
+const { webhook } = require("./env");
 const cron = require("node-cron");
 const { exec } = require("child_process");
 
-cron.schedule(
-  // 분 시간 일 월 요일 (월 = 1)
-  "22 1 * * *",
-  () => {
-    console.log("Sending curl request...");
-    const json = '{"content": "alarm is gone"}';
+function alarm(time, message, isSingleUse) {
+  const messageJson = JSON.stringify({ content: message });
 
-    exec(
-      `curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d ${json} webhook`,
-      (error, stdout, stderr) => {
+  console.log(messageJson, typeof messageJson);
+
+  cron.schedule(
+    // 분 시간 일 월 요일 (월 = 1)
+    time,
+    () => {
+      console.log("Sending curl request...", messageJson, webhook);
+      console.log(JSON.parse(messageJson));
+
+      const command = `curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d '${messageJson}' ${webhook}`;
+
+      console.log(command);
+      exec(command, (error, stdout, stderr) => {
         if (error) {
           console.error(`exec error: ${error}`);
           return;
         }
         console.log(`stdout: ${stdout}`);
         console.error(`stderr: ${stderr}`);
-      }
-    );
-  },
-  {
-    timezone: "Asia/Seoul",
-  }
-);
+      });
+    },
+    {
+      timezone: "Asia/Seoul",
+    }
+  );
+}
+
+alarm("* * * * *", "sell");
